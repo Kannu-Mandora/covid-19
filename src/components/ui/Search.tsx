be5f48c43bb82bace89a5f8ba0697f36bc3d594f
“Kannu-Mandora” <mandorakannu@gmail.com>
2023-01-26 19:49:17 +0530

    add pointer event to image

--- src/components/ui/Search.tsx
+++ src/components/ui/Search.tsx
@@ -53,21 +53,21 @@ function Search() {
     <>
       {/* Cards */}
       <div className="flexColCenter gap-3 h-80 bg-gradient-to-b from-[color:var(--card-linear-1)] to-[color:var(--card-linear-2)] m-5 rounded shadow-2xl">
-        <img src={logo} alt="Covid" className="w-20 animate-rotate" />
-        <h1 className="text-white text-2xl">Statistics</h1>
-        <span className="text-gray-300">Search Country Covid Stats</span>
+        <img src={logo} alt="Covid" className="w-20 animate-rotate pointer-events-none" />
+        <h1 className="text-white text-2xl select-none">Statistics</h1>
+        <span className="text-gray-300 select-none">Search Country Covid Stats</span>
         <input
           type="search"
           name="search"
           id="search-country"
           value={getCountry}
           onChange={(e) => setCountry(e.target.value)}
-          className="bg-[color:var(--darker-box)] rounded p-2 text-white placeholder:text-gray-300 outline-none"
+          className="bg-[color:var(--darker-box)] rounded p-2 text-white placeholder:text-gray-300 outline-none pointer-events-auto"
           placeholder="Search Country"
         />
         <button
           type="submit"
-          className="bg-[color:var(--darker-box)] px-3 py-1 my-1 text-white rounded"
+          className="bg-[color:var(--darker-box)] px-3 py-1 my-1 text-white rounded pointer-events-auto"
           onClick={getCountryStatistics}
         >
           Search
