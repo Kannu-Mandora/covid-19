43d3ef274aeacb2f3fd14e24d948800551e49e16
“Kannu-Mandora” <mandorakannu@gmail.com>
2023-01-26 19:51:37 +0530

    Add cursor-dafault & select-none class in footer

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-[color:var(--darker-box)] p-3 fixed bottom-0 w-screen flexColCenter" >
+    <div className="bg-[color:var(--darker-box)] p-3 fixed bottom-0 w-screen flexColCenter cursor-default select-none" >
       <p className='text-white text-center' >Made with <span className='text-red-400'>&hearts;</span> by <a href='https://github.com/Kannu-Mandora' target="_blank" className='hover:underline underline-offset-4 transition-all ease-in-out' >Kannu Mandora</a></p>
     </div>
   )
